def8a0013651cdfe346115d8d411101291d812ef
AlestarAndrei <robert.mocanu@5psolutions.eu>
2022-04-14 17:36:27 +0300

    17 orders http://pm.5psolutions.net/public/index.php?path_info=projects/training-robert-mocanu/tasks/17

--- app/Models/OrderProduct.php
+++ app/Models/OrderProduct.php
@@ -9,9 +9,16 @@ class OrderProduct extends Model
 {
     use HasFactory;
 
-
+    protected $fillable = [
+        'price','order_id','product_id'
+    ];
     public function product()
     {
         return $this->belongsTo(Product::class);
+    }
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
     }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -15,7 +15,7 @@ class Product extends Model
 
     public function orders()
     {
-        return $this->belongsToMany(Order::class);
+        return $this->belongsToMany(Order::class)->withPivot('price');
     }
 
 
